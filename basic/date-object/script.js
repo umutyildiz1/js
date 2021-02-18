7f525d60bdeedbe1ebdccfe605c224b5b46aefca
umutyildiz <umut.yildiz05@hotmail.com>
2021-02-18 19:34:36 +0300

    date

---
+++ basic/date-object/script.js
@@ -0,0 +1,23 @@
+var date = new Date();
+var customDate = new Date(2020,6,4)
+//Set Methods
+
+
+console.log(date.setMonth(7));
+console.log(date.setFullYear(2020));
+
+//Get Methods
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.toJSON());
+
+
+
+
+
+//typeof and value
+console.log(customDate)
+console.log(typeof date);
+console.log(date);
