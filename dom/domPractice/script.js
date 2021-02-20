e228a1705d78609ea58c4072f6c8bcd29a8ee0ee
umutyildiz <umut.yildiz05@hotmail.com>
2021-02-20 23:49:06 +0300

    dom

---
+++ dom/domPractice/script.js
@@ -0,0 +1,23 @@
+let val;
+
+val = window.document;
+val = document.all;
+val = document.all.length;
+val = document.all[10];
+val = document.head;
+val = document.body;
+val = document.anchors;
+val = document.URL;
+val = document.domain;
+val = document.images;
+val = document.forms;//we take all form in html page
+val = document.title;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.scripts[2];
+val = document.scripts[2].getAttribute("src");
+
+
+
+console.log(val)
